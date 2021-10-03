68f7f003fb2d66e4b248ae1030b6768ef6543a0e
Ren <hodorugi@gmail.com>
2021-10-03 01:04:00 -0700

    fix toolbar (?)

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,5 +1,11 @@
 function colorMode(){
     $('[character]').toggleClass('dark');
+    $('#modechange').on('click', function() {
+        $('#modechange').text('light_mode');
+    });
+    $('#modechange').on('click', function() {
+        $('#modechange').text('dark_mode');
+    });
 }
 $(document).ready(function() {
     colorMode();
