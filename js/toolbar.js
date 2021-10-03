08538b7d23a590aa31445a22b9de190ff3247ec0
Ren <hodorugi@gmail.com>
2021-10-03 01:04:54 -0700

    commented out js

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,11 +1,8 @@
 function colorMode(){
     $('[character]').toggleClass('dark');
-    $('#modechange').on('click', function() {
+    /* $('#modechange').on('click', function() {
         $('#modechange').text('light_mode');
-    });
-    $('#modechange').on('click', function() {
-        $('#modechange').text('dark_mode');
-    });
+    }); */
 }
 $(document).ready(function() {
     colorMode();
