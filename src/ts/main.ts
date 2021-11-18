691f143024b72c567b6a052a939800659053ad12
mgn-yuuki <58155530+mgn-yuuki@users.noreply.github.com>
2021-11-18 12:29:12 +0700

    implemented webpack

---
+++ src/ts/main.ts
@@ -0,0 +1,123 @@
+import $ from "jquery";
+import "../scss/main.scss";
+let mashiroConfig;
+
+function saveConfig() {
+    localStorage.setItem("mashiroCookie", JSON.stringify(mashiroConfig));
+}
+
+function colorFill() {
+    $("[character]").toggleClass("fill");
+    $("#dark-toggle").toggle();
+    $("#light-toggle").toggle();
+    mashiroConfig.darkColors = !mashiroConfig.darkColors;
+    saveConfig();
+}
+
+function sliderDrop() {
+    $(".toolbar-wrapper").toggleClass("showSlider");
+}
+
+function cardLightboxInitialize() {
+    $("body").append(`<div class="lightbox__dim" style="display: none;">
+        <div class="lightbox-content"></div>
+    </div>`);
+}
+
+function minitalkInitialize() {
+    $(".minitalk-option_header").click(function () {
+        $(this).next().slideToggle();
+        $(this).toggleClass("tab-header__open");
+    });
+    $(".tabber a").click(function () {
+        $(this).parent().siblings().removeClass("active");
+        $(this).parent().addClass("active");
+        $(this).parents(".minitalk-option_content").children("div").hide();
+        $(this)
+            .parents(".minitalk-option_content")
+            .children(`div[data-tab="${$(this).attr("data-tab")}"]`)
+            .show();
+    });
+}
+
+function storyCoverInitialize() {
+    cardLightboxInitialize();
+    $(".tab-header,.mt-header").click(function () {
+        $(this).next().slideToggle();
+        $(this).toggleClass("tab-header__open");
+    });
+    $(".cards-item").click(function () {
+        $("body").addClass("lightbox-visible");
+        $(".lightbox-content").html($(this).html()).parent().fadeToggle(200);
+    });
+    $(".lightbox__dim")
+        .click(function () {
+            $("body").removeClass("lightbox-visible");
+            $(".lightbox-content").parent().fadeOut(200);
+        })
+        .children()
+        .click(function (e) {
+            return false;
+        });
+    $(".card-pair-wrapper").click(function () {
+        $(".card-pair-wrapper").toggleClass("bloomed");
+    });
+}
+
+function toolbarInitialize() {
+    let mashiroCookie = localStorage.getItem("mashiroCookie");
+    const fontSizes = ["smallest", "smaller", "", "bigger", "biggest"];
+
+    if (!mashiroCookie) {
+        const defaultMashiroConfig = {
+            darkColors: false,
+            // Slider range is 1-5, but font size array indices are 0-4
+            fontSize: fontSizes.indexOf("") + 1
+        };
+        mashiroConfig = defaultMashiroConfig;
+        saveConfig();
+        mashiroCookie = JSON.stringify(defaultMashiroConfig);
+    }
+
+    mashiroConfig = JSON.parse(mashiroCookie);
+
+    const handleSliderChange = event => {
+        const fontSize = fontSizes[event.target.value - 1];
+        $("[character]").removeClass(fontSizes.filter(size => !!size));
+        $("[character]").addClass(fontSize);
+        mashiroConfig.fontSize = event.target.value;
+        saveConfig();
+    };
+    $("input.slider").on("change", handleSliderChange);
+
+    // Initialize fill mode based on config value
+    $("[character]").toggleClass("fill", mashiroConfig.darkColors);
+    $("#dark-toggle").toggle(mashiroConfig.darkColors);
+    $("#light-toggle").toggle(!mashiroConfig.darkColors);
+
+    if (mashiroConfig.fontSize) {
+        $("input.slider").val(mashiroConfig.fontSize);
+        // Need to manually invoke event listener
+        $("input.slider").change();
+    }
+}
+
+function footnoteInitialize() {
+    tippy.setDefaultProps({
+        maxWidth: 350,
+        allowHTML: true,
+        animation: "shift-away",
+        delay: [50, 200],
+        zIndex: 99,
+        moveTransition: "transform 0.2s ease",
+        interactive: true,
+    });
+    tippy("[data-tippy-content]");    
+}
+
+$(document).ready(function () {
+    minitalkInitialize();
+    storyCoverInitialize();
+    toolbarInitialize();
+    footnoteInitialize();
+});
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    entry: "./src/ts/main.ts",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+        }),
+    ],
+};
